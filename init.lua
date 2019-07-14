1f94ced2465546e509f849fc5ba3a09cd0de0cd0
Matheus Pinheiro <mp-pinheiro@hotmail.com>
2019-07-13 21:21:15 -0300

    Murdered init.

--- init.lua
+++
@@ -1,4 +0,0 @@
-package.loaded["Class"] = require("Utils.Class")
-package.loaded["Utils"] = require("Utils.Utils")
-package.loaded["Tests"] = require("Utils.Tests")
-package.loaded["DatabaseConnection"] = require("Utils.DatabaseConnection")
