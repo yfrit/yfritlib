60752ca6cd6cb12d38153dea440f5cc1abb56e80
preischadt <lucas_p@outlook.com>
2019-10-18 23:15:40 -0300

    Implement callback with parameters

--- CallbackList.lua
+++ CallbackList.lua
@@ -2,6 +2,7 @@
 Implements a list, where callbacks can be inserted and executed.
 --]]
 local Class = require("YfritLib.Class")
+local Utils = require("YfritLib.Utils")
 
 local CallbackList =
 	Class.new(
@@ -11,13 +12,18 @@ local CallbackList =
 	end
 )
 
-function CallbackList:insert(callback)
-	table.insert(self.callbacks, callback)
+function CallbackList:insert(callback, ...)
+	self.callbacks[callback] = {...}
 end
 
 function CallbackList:execute(...)
-	for _, callback in ipairs(self.callbacks) do
-		callback(...)
+	for callback, insertParams in pairs(self.callbacks) do
+		-- concatanate paramaters received on insert with parameters received on execute
+		local allParams = Utils.shallowCopy(insertParams)
+		for _, executeParam in ipairs({...}) do
+			table.insert(allParams, executeParam)
+		end
+		callback(unpack(allParams))
 	end
 end
 
