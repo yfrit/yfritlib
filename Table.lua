6cd862672e70000f8ddb03c9f1fec782e81aa1ae
preischadt <lucas_p@outlook.com>
2019-12-09 23:17:10 -0300

    Implement Table.reduce

--- Table.lua
+++ Table.lua
@@ -4,25 +4,35 @@
 local Table = {}
 
 function Table.append(t1, t2)
-	local newTable = {}
+    local newTable = {}
 
-	for _, value in ipairs(t1) do
-		newTable[#newTable + 1] = value
-	end
-	for _, value in ipairs(t2) do
-		newTable[#newTable + 1] = value
-	end
+    for _, value in ipairs(t1) do
+        newTable[#newTable + 1] = value
+    end
+    for _, value in ipairs(t2) do
+        newTable[#newTable + 1] = value
+    end
 
-	return newTable
+    return newTable
 end
 
 -- TODO write tests for this
 function Table.shallowCopy(t)
-	local tCopy = {}
-	for i, v in pairs(t) do
-		tCopy[i] = v
-	end
-	return tCopy
+    local tCopy = {}
+    for i, v in pairs(t) do
+        tCopy[i] = v
+    end
+    return tCopy
+end
+
+function Table.reduce(t, callback)
+    local total = t[1]
+
+    for i = 2, #t do
+        total = callback(total, t[i])
+    end
+
+    return total
 end
 
 return Table
--- spec/Table_spec.lua
+++ spec/Table_spec.lua
@@ -1,37 +1,53 @@
 require("LocalRockInit")
 
 insulate(
-	"#Table",
-	function()
-		local Table
-		setup(
-			function()
-				Table = require("Table")
-			end
-		)
-		it(
-			"append TwoTables ReturnsANewTable",
-			function()
-				local t1 = {}
-				local t2 = {}
+    "#Table",
+    function()
+        local Table
+        setup(
+            function()
+                Table = require("Table")
+            end
+        )
+        it(
+            "append TwoTables ReturnsANewTable",
+            function()
+                local t1 = {}
+                local t2 = {}
 
-				local t3 = Table.append(t1, t2)
+                local t3 = Table.append(t1, t2)
 
-				assert.are_not_equal(t1, t3)
-				assert.are_not_equal(t2, t3)
-			end
-		)
-		it(
-			"append TwoTables JoinsTablesContents",
-			function()
-				local t1 = {1, true}
-				local t2 = {3, "potato"}
+                assert.are_not_equal(t1, t3)
+                assert.are_not_equal(t2, t3)
+            end
+        )
+        it(
+            "append TwoTables JoinsTablesContents",
+            function()
+                local t1 = {1, true}
+                local t2 = {3, "potato"}
 
-				local t3 = Table.append(t1, t2)
+                local t3 = Table.append(t1, t2)
 
-				local expectedTable = {1, true, 3, "potato"}
-				assert.are_same(expectedTable, t3)
-			end
-		)
-	end
+                local expectedTable = {1, true, 3, "potato"}
+                assert.are_same(expectedTable, t3)
+            end
+        )
+        it(
+            "reduce TableAndFunction ReducesTableToSingleValue",
+            function()
+                local t = {2, 3, 4}
+
+                local multiplication =
+                    Table.reduce(
+                    t,
+                    function(a, b)
+                        return a * b
+                    end
+                )
+
+                assert.are_equal(2 * 3 * 4, multiplication)
+            end
+        )
+    end
 )
