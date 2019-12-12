e91d2ec6f8ef8d4518d97112030636a0b1bf3a94
preischadt <lucas_p@outlook.com>
2019-12-11 23:50:15 -0300

    Only call promise callbacks once if completed multiple times

--- Promise.lua
+++ Promise.lua
@@ -6,42 +6,44 @@ local CallbackList = require("YfritLib.CallbackList")
 local Utils = require("YfritLib.Utils")
 
 local Promise =
-	Class.new(
-	{},
-	function(self)
-		self.isCompleted = false
-		self.callbackList = CallbackList:new()
-	end
+    Class.new(
+    {},
+    function(self)
+        self.isCompleted = false
+        self.callbackList = CallbackList:new()
+    end
 )
 
 function Promise:complete(...)
-	self.isCompleted = true
-	self.parameters = {...}
-	self.callbackList:execute(...)
+    if not self.isCompleted then
+        self.isCompleted = true
+        self.parameters = {...}
+        self.callbackList:execute(...)
+    end
 end
 
 function Promise:onComplete(callback)
-	if self.isCompleted then
-		--if promise already completed, executed callback immediately
-		callback(unpack(self.parameters))
-	else
-		--otherwise, save to execute lates
-		self.callbackList:insert(callback)
-	end
+    if self.isCompleted then
+        --if promise already completed, executed callback immediately
+        callback(unpack(self.parameters))
+    else
+        --otherwise, save to execute lates
+        self.callbackList:insert(callback)
+    end
 end
 
 function Promise:await()
-	if not self.isCompleted then
-		--only pause coroutine if promise didn't complete yet
-		local co = coroutine.running()
-		self:onComplete(
-			function()
-				Utils.resumeCoroutine(co)
-			end
-		)
-		coroutine.yield()
-	end
-	return unpack(self.parameters)
+    if not self.isCompleted then
+        --only pause coroutine if promise didn't complete yet
+        local co = coroutine.running()
+        self:onComplete(
+            function()
+                Utils.resumeCoroutine(co)
+            end
+        )
+        coroutine.yield()
+    end
+    return unpack(self.parameters)
 end
 
 return Promise
